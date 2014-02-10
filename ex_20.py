62a8bb1f60587c5a57e1c9b02488975a44c3f855
fanyan1120 <fanyan1120@yahoo.com>
2014-02-10 18:37:13 +0800

    add ex from 20 to 25
    add ex from 20 to 25

---
+++ ex_20.py
@@ -0,0 +1,29 @@
+﻿from sys import argv
+script, input_file = argv
+
+def print_all(f):
+    print f.read()
+
+def rewind(f):
+    f.seek(0)
+
+def print_a_line(row_num, f):
+    print row_num, f.readline()
+
+current_file = open(input_file)
+print "==========whole file=========="
+print_all(current_file)
+
+print "==========rewind=========="
+rewind(current_file)
+
+print "==========three lines=========="
+line = 1
+print "print line %d" %line
+print_a_line(line, current_file)
+line += 1
+print "print line %d" %line
+print_a_line(line, current_file)
+line += 1
+print "print line %d" %line
+print_a_line(line, current_file)
---
+++ ex_24.py
@@ -0,0 +1,30 @@
+print "print ' in \""
+print 'print \' in \' \nprint \\, print enter: \n print tab between aaa: aaa\taaa'
+
+aaa = """
+\tprint a tab
+balabalabala
+print enter:\n in a lien
+print enter and tab:\n\t\tbala
+"""
+print "================"
+print aaa
+print "================"
+
+five = 10 - 2 + 3 - 6
+print "This should be five: %s" % five
+
+def secret_formula(started):
+    jelly_beans = started * 500
+    jars = jelly_beans / 400
+    crates = jars / 100
+    return jelly_beans, jars, crates
+
+start_point = 6003
+beans, jars, crates = secret_formula(start_point)
+print "secret_formula = %d" % start_point
+print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
+
+start_point = start_point / 10
+print "secret_formula = %d" % start_point
+print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
